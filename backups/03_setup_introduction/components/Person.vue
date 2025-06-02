9e6e8ef38188af093091b2dc46f8aaa9947543be
ljyGmail <liangjy881@gmail.com>
2025-06-02 22:49:56 +0900

    08 setup的返回值

--- backups/03_setup_introduction/components/Person.vue
+++ backups/03_setup_introduction/components/Person.vue
@@ -37,10 +37,7 @@ export default {
       alert(tel)
     }
 
-    // 将数据、方法交出去，模板中才可以使用
     return { name, age, changeName, changeAge, showTel }
-    // setup的返回值也可以是一个渲染函数
-    // return () => '哈哈'
   },
 }
 </script>
