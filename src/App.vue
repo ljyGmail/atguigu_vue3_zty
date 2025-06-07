f62f9aeee8b1b4f70b6dd13e3dbf461ac3058cc8
liangjinyong <liangjy881@gmail.com>
2025-06-07 12:55:41 +0900

    28 Vue3的生命周期

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
-  <!-- 务必看懂下面这一行代码 -->
-  <!-- <h2 a="1+1" :b="1 + 1" c="x" :d="x" ref="qwe">测试</h2> -->
-  <Person a="哈哈" :list="personList" />
+  <Person v-if="isShow" />
 </template>
 
 <script lang="ts" setup name="App">
 import Person from './components/Person.vue'
-import { reactive } from 'vue'
-import { type Persons } from '@/types'
+import { ref, onMounted } from 'vue'
 
-let x = 9
+let isShow = ref(true)
 
-let personList = reactive<Persons>([
-  { id: 'vue01', name: '张三', age: 18 },
-  { id: 'vue02', name: '李四', age: 20 },
-  { id: 'vue03', name: '王五', age: 22 },
-])
+// 挂载完毕
+onMounted(() => {
+  console.log('父--挂载完毕')
+})
 </script>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,29 +1,61 @@
 <template>
   <div class="person">
-    <ul>
-      <li v-for="p in list" :key="p.id">{{ p.name }} -- {{ p.age }}</li>
-    </ul>
+    <h2>当前求和为: {{ sum }}</h2>
+    <button @click="add">点我sum+1</button>
   </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { withDefaults } from 'vue'
-import { type Persons } from '@/types'
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from 'vue'
 
-// 只接收list
-// defineProps(['list'])
+// 数据
+let sum = ref(0)
 
-// 接收list + 限制类型
-// defineProps<{list:Persons}>()
+// 方法
+function add() {
+  sum.value += 1
+}
 
-// 接收list + 限制类型 + 限制必要性 + 指定默认值
-withDefaults(defineProps<{ list?: Persons }>(), {
-  list: () => [{ id: 'ljy01', name: '康师傅', age: '19' }],
+// 创建
+console.log('创建')
+
+// 挂载前
+onBeforeMount(() => {
+  console.log('挂载前')
 })
 
-// 接收list同时将props保存起来
-// let x = defineProps(['list'])
-// console.log(x.list)
+// 挂载完毕
+onMounted(() => {
+  console.log('子--挂载完毕')
+})
+
+// 更新前
+onBeforeUpdate(() => {
+  console.log('更新前')
+})
+
+// 更新完毕
+onUpdated(() => {
+  console.log('更新完毕')
+})
+
+// 卸载前
+onBeforeUnmount(() => {
+  console.log('卸载前')
+})
+
+// 卸载完毕
+onUnmounted(() => {
+  console.log('卸载完毕')
+})
 </script>
 <style scoped>
 .person {
