b48f9d791c79c026190586eb9a0ca93af293eadf
ljyGmail <95560385+ljyGmail@users.noreply.github.com>
2025-06-09 13:45:34 +0900

    31 路由 基本切换效果

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img src="http://www.atguigu.com/images/index_new/logo.png" alt="" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
---
+++ src/components/News.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="news">
+    <ul>
+      <li><a href="#">新闻001</a></li>
+      <li><a href="#">新闻002</a></li>
+      <li><a href="#">新闻003</a></li>
+      <li><a href="#">新闻004</a></li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+/* 新闻 */
+.news{
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+}
+
+.news ul{
+  margin-top: 30px;
+  list-style: none;
+  padding-left: 10px;
+}
+</style>
