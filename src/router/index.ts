2a09952fa4505f12a2f73f3f41e3f5d79379fefd
ljyGmail <95560385+ljyGmail@users.noreply.github.com>
2025-06-09 14:57:19 +0900

    36 路由 路由嵌套

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,15 @@
 // 创建一个路由器，并暴露出去
 // 第一步: 引入createRouter
-import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 // 引入一个一个可能要呈现的组件
 import Home from '@/pages/Home.vue'
 import News from '@/pages/News.vue'
 import About from '@/pages/About.vue'
+import Detail from '@/pages/Detail.vue'
 
 // 第二步: 创建路由器
 const router = createRouter({
-  history: createWebHashHistory(), // 路由器的工作模式(稍后讲解)
+  history: createWebHistory(), // 路由器的工作模式
   routes: [
     // 一个一个的路由规则
     {
@@ -20,6 +21,12 @@ const router = createRouter({
       name: 'xinwen',
       path: '/news',
       component: News,
+      children: [
+        {
+          path: 'detail',
+          component: Detail,
+        },
+      ],
     },
     {
       name: 'guanyu',
