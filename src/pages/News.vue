5c0f2c8d226cdd64a77de819ec61d9c811f70869
ljyGmail <95560385+ljyGmail@users.noreply.github.com>
2025-06-10 10:16:18 +0900

    41 路由 编程式路由导航

--- src/pages/News.vue
+++ src/pages/News.vue
@@ -3,13 +3,7 @@
     <!-- 导航区 -->
     <ul>
       <li v-for="news in newsList" :key="news.id">
-        <!-- 第一种写法 -->
-        <!--
-        <RouterLink :to="`/news/detail/${news.id}/${news.title}/${news.content}`">
-          {{ news.title }}
-        </RouterLink>
-        -->
-        <!-- 第二种写法 -->
+        <button @click="showNewsDetail(news)">查看新闻</button>
         <RouterLink
           :to="{
             name: 'xiang', // 这里只能用路由的名字，不能用path
@@ -32,7 +26,7 @@
 
 <script setup lang="ts">
 import { reactive } from 'vue'
-import { RouterView, RouterLink } from 'vue-router'
+import { RouterView, RouterLink, useRouter } from 'vue-router'
 
 const newsList = reactive([
   { id: 'vue01', title: '很好的抗癌食物', content: '西兰花' },
@@ -40,6 +34,21 @@ const newsList = reactive([
   { id: 'vue03', title: '震惊，万万没想到', content: '明天是星期一' },
   { id: 'vue04', title: '好消息！好消息！', content: '快过年了' },
 ])
+
+const router = useRouter()
+
+interface NewsInter {
+  id: string
+  title: string
+  content: string
+}
+
+function showNewsDetail(news: NewsInter) {
+  router.replace({
+    name: 'xiang',
+    query: { id: news.id, title: news.title, content: news.content },
+  })
+}
 </script>
 
 <style scoped>
