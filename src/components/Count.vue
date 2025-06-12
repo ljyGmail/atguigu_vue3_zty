cb34c65a9c3f8ace405c6f2cafd996041a5bb86b
ljyGmail <95560385+ljyGmail@users.noreply.github.com>
2025-06-12 11:03:37 +0900

    48 storeToRefs

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="count">
-    <h2>当前求和为: {{ countStore.sum }}</h2>
-    <h3>欢迎来到: {{ countStore.school }}，坐落于: {{ countStore.address }}</h3>
+    <h2>当前求和为: {{ sum }}</h2>
+    <h3>欢迎来到: {{ school }}，坐落于: {{ address }}</h3>
     <select v-model.number="n">
       <option value="1">1</option>
       <option value="2">2</option>
@@ -13,32 +13,29 @@
 </template>
 
 <script setup lang="ts" name="Count">
-import { ref, reactive } from 'vue'
+import { ref, toRefs } from 'vue'
 // 引入userCountStore
 import { useCountStore } from '@/store/count'
+import { storeToRefs } from 'pinia'
 // 使用useCountStore，得到一个专门保存count相关的store
 const countStore = useCountStore()
+
+// storeToRefs只用关注store中的数据，不会对方法进行ref包裹
+const { sum, school, address } = storeToRefs(countStore)
+// console.log('@@@@@', storeToRefs(countStore))
+// console.log('!!!!!', toRefs(countStore)) // toRefs这种方式代价太大
 
 // 数据
 let n = ref(1) // 用户选择的数字
 
 // 方法
 function add() {
-  // 第一种修改方式
-  // countStore.sum += 1
-  // 第二种修改方式
-  /*
-  countStore.$patch({
-    sum: 888,
-    school: '尚硅谷',
-    address: '北京',
-  })
-   */
-  // 第三种修改方式
   countStore.increment(n.value)
 }
 
-function minus() {}
+function minus() {
+  countStore.sum -= n.value
+}
 </script>
 
 <style scoped>
--- src/components/LoveTalk.vue
+++ src/components/LoveTalk.vue
@@ -8,36 +8,13 @@
 </template>
 
 <script setup lang="ts" name="LoveTalk">
-import { reactive } from 'vue'
-import axios from 'axios'
-import { nanoid } from 'nanoid'
 import { useTalkStore } from '@/store/loveTalk'
 
 const talkStore = useTalkStore()
 
-const loveTalkDB = [
-  { id: 'vue04', title: '你的脸上有点东西，有什么?有点漂亮。' },
-  { id: 'vue05', title: '我可以不为别人难过，但你不是别人，你是我的人。' },
-  { id: 'vue06', title: '有些事不用在一晚内做完的，我们又不赶时间，可以每晚都做一做' },
-  { id: 'vue07', title: '我在找一匹马。什么马?你的微信号码。' },
-  { id: 'vue08', title: '你是我最爱的女孩，再多奶茶都不换那种。' },
-  { id: 'vue09', title: '我怀疑你的本质是一本书，不然为什么让我越看越想睡。' },
-]
-
 // 方法
-async function getLoveTalk() {
-  /*
-  // 发请求，下面这行的写法是: 连续解构赋值+重命名
-  let {
-    data: { content: title },
-  } = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json')
-  // 把请求回来的字符串，包装成一个对象
-  let obj = { id: nanoid(), title }
-  console.log(obj)
-   */
-  // 放到数组中
-  // talkList.unshift(obj)
-  // talkList.unshift(loveTalkDB[Math.floor(Math.random() * loveTalkDB.length)])
+function getLoveTalk() {
+  talkStore.getATalk()
 }
 </script>
 
