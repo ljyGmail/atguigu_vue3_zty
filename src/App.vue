3574a4eb0ef24f1c57e4937c0a80c497c2fc9716
liangjinyong <liangjy881@gmail.com>
2025-06-13 11:00:07 +0900

    54 组件通信 方式3 mitt

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@
 				<!-- 导航区 -->
 				<router-link active-class="active" class="list-group-item" to="/props">1. props</router-link>
 				<router-link active-class="active" class="list-group-item" to="/event">2. 自定义事件</router-link>
-        <!--
 				<router-link active-class="active" class="list-group-item" to="/mitt">3. mitt</router-link>
+        <!--
 				<router-link active-class="active" class="list-group-item" to="/model">4. v-model</router-link>
 				<router-link active-class="active" class="list-group-item" to="/attrs">5. $attrs</router-link>
 				<router-link active-class="active" class="list-group-item" to="/ref-parent">6. <span class="small">$refs、$parent</span></router-link>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Props from '@/pages/01_props/Father.vue'
 import Event from '@/pages/02_custom-event/Father.vue'
-// import Bus from '@/pages/03_mitt/Father.vue'
+import Bus from '@/pages/03_mitt/Father.vue'
 // import Model from '@/pages/04_v-model/Father.vue'
 // import AttrsListeners from '@/pages/05_$attrs/Father.vue'
 // import RefChildrenParent from '@/pages/06_$refs-$parent/Father.vue'
@@ -20,11 +20,11 @@ export default createRouter({
       path: '/event',
       component: Event,
     },
-    /*
     {
       path: '/mitt',
       component: Bus,
     },
+    /*
     {
       path: '/model',
       component: Model,
