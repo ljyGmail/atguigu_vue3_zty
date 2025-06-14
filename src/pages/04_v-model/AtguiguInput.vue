a21433af0d8eea98a3566052814fb9fafc656a96
ljyGmail <95560385+ljyGmail@users.noreply.github.com>
2025-06-14 13:30:38 +0900

    56 组件通信 方式4 v-model的细节

--- src/pages/04_v-model/AtguiguInput.vue
+++ src/pages/04_v-model/AtguiguInput.vue
@@ -1,13 +1,19 @@
 <template>
   <input
     type="text"
-    :value="modelValue"
-    @input="emit('update:modelValue', (<HTMLInputElement>$event.target).value)"
+    :value="ming"
+    @input="emit('update:ming', (<HTMLInputElement>$event.target).value)"
+  />
+  <br />
+  <input
+    type="text"
+    :value="mima"
+    @input="emit('update:mima', (<HTMLInputElement>$event.target).value)"
   />
 </template>
 <script setup lang="ts" name="AtguiguInput">
-defineProps(['modelValue'])
-const emit = defineEmits(['update:modelValue'])
+defineProps(['ming', 'mima'])
+const emit = defineEmits(['update:ming', 'update:mima'])
 </script>
 
 <style scoped>
--- src/pages/04_v-model/Father.vue
+++ src/pages/04_v-model/Father.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="father">
     <h3>父组件</h3>
+    <h4>{{ username }}</h4>
+    <h4>{{ password }}</h4>
     <!-- v-model用在html标签上 -->
     <!-- <input type="text" v-model="username" /> -->
     <!--
@@ -11,8 +13,11 @@
     />
     -->
     <!-- v-model用在组件标签上 -->
-    <AtguiguInput v-model="username" />
+    <!-- <AtguiguInput v-model="username" /> -->
     <!-- <AtguiguInput :modelValue="username" @update:modelValue="username = $event" /> -->
+
+    <!-- 修改modelValue -->
+    <AtguiguInput v-model:ming="username" v-model:mima="password" />
   </div>
 </template>
 
@@ -22,6 +27,7 @@ import AtguiguInput from './AtguiguInput.vue'
 
 // 数据
 let username = ref('zhangsan')
+let password = ref('123456')
 </script>
 
 <style scoped>
