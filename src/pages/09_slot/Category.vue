5f15d65c64217733b973230019e27ceaede9488b
ljyGmail <liangjy881@gmail.com>
2025-06-19 15:01:54 +0900

    62 插槽 具名插槽

--- src/pages/09_slot/Category.vue
+++ src/pages/09_slot/Category.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="category">
-    <h2>{{ title }}</h2>
-    <slot>默认内容</slot>
+    <slot name="s1">默认内容1</slot>
+    <slot name="s2">默认内容2</slot>
   </div>
 </template>
 
 <script setup lang="ts" name="Category">
-defineProps(['title'])
 </script>
 
 <style scoped>
@@ -17,12 +16,5 @@ defineProps(['title'])
   padding: 10px;
   width: 200px;
   height: 300px;
-}
-
-h2 {
-  background-color: orange;
-  text-align: center;
-  font-size: 20px;
-  font-weight: 800;
 }
 </style>
--- src/pages/09_slot/Father.vue
+++ src/pages/09_slot/Father.vue
@@ -2,16 +2,31 @@
   <div class="father">
     <h3>父组件</h3>
     <div class="content">
-      <Category title="热门游戏列表">
-        <ul>
-          <li v-for="g in games" :key="g.id">{{ g.name }}</li>
-        </ul>
+      <Category>
+        <template v-slot:s2>
+          <ul>
+            <li v-for="g in games" :key="g.id">{{ g.name }}</li>
+          </ul>
+        </template>
+        <template v-slot:s1>
+          <h2>热门游戏列表</h2>
+        </template>
       </Category>
-      <Category title="今日我的自拍">
-        <img :src="imageUrl" alt="我的自拍" />
+      <Category>
+        <template #s2>
+          <img :src="imageUrl" alt="我的自拍" />
+        </template>
+        <template #s1>
+          <h2>今日我的自拍</h2>
+        </template>
       </Category>
-      <Category title="今日影视推荐">
-        <video :src="videoUrl" controls />
+      <Category>
+        <template #s2>
+          <video :src="videoUrl" controls />
+        </template>
+        <template #s1>
+          <h2>今日影视推荐</h2>
+        </template>
       </Category>
     </div>
   </div>
@@ -25,7 +40,7 @@ let games = reactive([
   { id: 'vue01', name: '英雄联盟' },
   { id: 'vue02', name: '王者荣耀' },
   { id: 'vue03', name: '红色警戒' },
-  { id: 'vue04', name: '斗罗大陆' }
+  { id: 'vue04', name: '斗罗大陆' },
 ])
 
 let imageUrl = ref('/images/myself.jpg')
@@ -47,5 +62,12 @@ let videoUrl = ref('/images/movie.mp4')
 img,
 video {
   width: 100%;
+}
+
+h2 {
+  background-color: orange;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 800;
 }
 </style>
