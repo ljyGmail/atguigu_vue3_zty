b073159d40f602dbb0c14b3965fcd186a692fa42
liangjinyong <liangjy881@gmail.com>
2025-06-25 00:45:50 +0900

    65 readonly与shallowReadonly

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,49 @@
 <template>
   <div class="app">
-    <h2>求和为: {{ sum }}</h2>
-    <h2>名字为: {{ person.name }}</h2>
-    <h2>年龄为: {{ person.age }}</h2>
-    <h2>汽车为: {{ car }}</h2>
-    <button @click="changeSum">sum+1</button>
-    <button @click="changeName">修改名字</button>
-    <button @click="changeAge">修改年龄</button>
-    <button @click="changePerson">修改整个人</button>
-    <span>|</span>
-    <button @click="changeBrand">修改品牌</button>
-    <button @click="changeColor">修改颜色</button>
-    <button @click="changeEngine">修改发动机</button>
+    <h2>当前sum1为: {{ sum1 }}</h2>
+    <h2>当前sum2为: {{ sum2 }}</h2>
+    <h2>当前car1为: {{ car1 }}</h2>
+    <h2>当前car2为: {{ car2 }}</h2>
+    <button @click="changeSum1">点我sum1+1</button>
+    <button @click="changeSum2">点我sum2+1</button>
+    <button @click="changeBrand2">修改品牌(car2)</button>
+    <button @click="changeColor2">修改颜色(car2)</button>
+    <button @click="changePrice2">修改价格(car2)</button>
   </div>
 </template>
 
 <script setup lang="ts" name="App">
-import { ref, reactive, shallowRef, shallowReactive } from 'vue'
+import { ref, reactive, readonly, shallowReadonly } from 'vue'
 
-let sum = shallowRef(0)
-let person = shallowRef({
-  name: '张三',
-  age: 18,
-})
-
-let car = shallowReactive({
+let sum1 = ref(0)
+let sum2 = readonly(sum1)
+let car1 = reactive({
   brand: '奔驰',
   options: {
     color: '红色',
-    engine: 'V8',
+    price: 100,
   },
 })
+let car2 = shallowReadonly(car1)
 
-function changeSum() {
-  sum.value += 1
+function changeSum1() {
+  sum1.value += 1
 }
 
-function changeName() {
-  person.value.name = '李四'
+function changeSum2() {
+  // sum2.value += 1 // sum2是不能修改的
 }
 
-function changeAge() {
-  person.value.age += 1
+function changeBrand2() {
+  car2.brand = '宝马'
 }
 
-function changePerson() {
-  person.value = { name: 'tony', age: 100 }
+function changeColor2() {
+  car2.options.color = '绿色'
 }
 
-/* ***************************** */
-function changeBrand() {
-  car.brand = '宝马'
-}
-
-function changeColor() {
-  car.options.color = '紫色'
-}
-
-function changeEngine() {
-  car.options.engine = 'V12'
+function changePrice2() {
+  car2.options.price += 10
 }
 </script>
 
